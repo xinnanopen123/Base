51371380c8964e5994ce93b7e6c48324f25d4667
xinnanopen123 <981305504@qq.com>
2016-04-18 09:52:03 +0800

    删除——垃圾代码

--- BaseFrmae/src/com/wechat/business/dao/IUserDao.java
+++
@@ -1,8 +0,0 @@
-package com.wechat.business.dao;
-
-import java.io.Serializable;
-import com.wechat.business.dao.base.IBaseDao;
-
-public interface IUserDao<T> extends IBaseDao<T, Serializable>{
-	
-}
